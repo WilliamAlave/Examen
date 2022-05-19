ba555ad645f504aea31673baadb84933a53ff59b
WilliamAlave <102809147+WilliamAlave@users.noreply.github.com>
2022-05-19 11:13:45 -0500

    Add files via upload
    Vaciar cola !!

---
+++ pregunta2.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+using namespace std;
+class ColaCircular
+{
+    //atributos
+    int tamanio = 5;
+    int elementos[5];
+    int indiceInicio;
+    int indiceFinal;
+    int cantidadElementos;
+public:
+    //construtor
+    ColaCircular()
+    {
+        indiceInicio = 0;
+        indiceFinal = 0;
+        cantidadElementos = 0;
+    }
+    //metodos
+    bool ColaLlena()
+    {
+        if (cantidadElementos >= tamanio)
+            return true;
+        else
+            return false;
+    }
+    bool ColaVacia()
+    {
+        if (cantidadElementos <= 0)
+            return true;
+        else
+            return false;
+    }
+    void Insertar(int elemento)
+    {
+        if (ColaLlena())
+        {
+            cout << "La cola esta llena" << endl;
+        }
+        else
+        {
+            cantidadElementos++;
+            elementos[indiceFinal] = elemento;
+            indiceFinal = indiceFinal + 1;
+            if (indiceFinal >= tamanio)
+                indiceFinal = 0;
+        }
+    }
+    int Quitar()
+    {
+        if (ColaVacia())
+        {
+            cout << "La cola esta vacia" << endl;
+        }
+        else
+        {
+            int valorRetornar = elementos[indiceInicio];
+            indiceInicio = indiceInicio + 1;
+            if (indiceInicio >= tamanio)
+                indiceInicio = 0;
+            cantidadElementos--;
+            return valorRetornar;
+        }
+    }
+    void Imprimir()
+    {
+        int indice = indiceInicio;
+        for (int i = 0; i < cantidadElementos; i++)
+        {
+            cout << elementos[indice] << "\t";
+            indice++;
+            if (indice >= tamanio)
+                indice = 0;
+        }
+        cout << endl;
+    }
+    //!vaciado de Cola
+    void vaciar(){
+        if(ColaVacia()){
+            cout<<"La  cola esta  vacia ";
+        }else{
+            while(cantidadElementos!=NULL){
+                cantidadElementos--;
+
+            }
+            indiceFinal = indiceInicio;
+            cout<<"Cola Vaciada"<<indiceFinal<<endl;
+        }
+
+    }
+
+};
+
+void Menu()
+{
+    cout << "1) Insertar elemento " << endl;
+    cout << "2) Quitar elemento " << endl;
+    cout << "3) Imprimir cola " << endl;
+    cout << "4) vaciar  cola"<<endl;
+    cout << "0) Salir " << endl;
+}
+
+int main()
+{
+    int opcion = 0;
+    int elemento = 0;
+    ColaCircular cola = ColaCircular();
+    do
+    {
+        Menu();
+        cin >> opcion;
+        switch (opcion)
+        {
+        case 1:
+            cout << "Ingrese el elemento" << endl;
+            cin >> elemento;
+            cola.Insertar(elemento);
+            break;
+        case 2:
+            cout << "El elemento quitado es: " << cola.Quitar() << endl;
+            break;
+        case 3:
+            cola.Imprimir();
+            break;
+        case 4:
+            cola.vaciar();
+            break;
+        default:
+            cout << "opción no valida" << endl;
+            break;
+        }
+        cout << "++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
+        cout << "++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
+    } while (opcion != 0);
+}
